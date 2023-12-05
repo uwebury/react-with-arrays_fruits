a5121c9cad486eace1df90acfe36ccef8a70800f
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-05 10:50:44 +0100

    Removed color prop in return statement

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ export default function App() {
 
   return (
     <div className="app">
-      {fruits.map(({ id, name, color }) => (
-        <Card key={id} name={name} color={color} />
+      {fruits.map(({ id, name }) => (
+        <Card key={id} name={name} />
       ))}
     </div>
   );
